b12fa49132cada3524aea71ccc6a8ca4fd9c30ce
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-03-07 03:21:48 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "s3bucketmss" {
+
+bucket = "s3-bucket-mss"
+
+acl = "private"
+
+versioning {
+
+enabled = true
+
+}
+
+lifecycle_rule {
+
+enabled = true
+
+transition {
+
+days = 10 storage_class = "STANDARD IA"
+
+}
+
+transition {
+
+days = 30 }
+
+storage_class = "GLACIER"
+
+}
+
+tags = {
+
+Name = "S3 Bucket MSS by Terraform" }
+
+}
