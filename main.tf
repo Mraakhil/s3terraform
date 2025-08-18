5f4d543c3f8e77eb7e83515aee7d77ec0254ce49
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-08-18 15:54:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,26 +10,6 @@ enabled = true
 
 }
 
-lifecycle_rule {
 
-enabled = true
-
-transition {
-
-days = 10 storage_class = "STANDARD IA"
-
-}
-
-transition {
-
-days = 30 }
-
-storage_class = "GLACIER"
-
-}
-
-tags = {
-
-Name = "S3 Bucket MSS by Terraform" }
 
 }
