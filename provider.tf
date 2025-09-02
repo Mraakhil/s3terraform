a63726ec6ec791857806bc0065533556fad416cd
Mohammad Aakhil <149184615+Mraakhil@users.noreply.github.com>
2025-09-02 12:39:31 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
  provider "aws" {
   region     = "ap-south-1a"
-  access_key = "AKIAUPH5S4L3V77WN3M6"
-  secret_key = "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"
+  access_key = "AKIAUPH5S4L35G7KMPVZ"
+  secret_key = "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"
 }
